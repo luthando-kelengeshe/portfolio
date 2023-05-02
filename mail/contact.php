92f41e6558164c5fd8ad2d36ff3973084beeb6d1
devotedmedia-admin <lkelengeshe@outlook.com>
2023-05-02 13:15:51 +0200

    Update contact.php

--- mail/contact.php
+++ mail/contact.php
@@ -15,6 +15,10 @@ $body = "You have received a new message from your website contact form.\n\n"."H
 $header = "From: $email";
 $header .= "Reply-To: $email";	
 
+mail($to, $subject, $body, $header);
+
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
-?>
+
+?> 
+
