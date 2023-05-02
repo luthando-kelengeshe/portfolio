34af21e8a0dc35316d3dffcd1c472487089a1e9d
devotedmedia-admin <lkelengeshe@outlook.com>
2023-05-02 16:11:06 +0200

    Contact form

--- mail/contact.php
+++ mail/contact.php
@@ -1,24 +1,25 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    http_response_code(400);
+    exit();
+  }
+  
+  $name = trim($_POST['name']);
+  $email = trim($_POST['email']);
+  $m_subject = trim($_POST['subject']);
+  $message = trim($_POST['message']);
+  
+  $to = "lkelengeshe@outlook.com"; 
+  $subject = "$m_subject:  $name";
+  $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+  $header = "From: $email";
+  $header .= "Reply-To: $email";	
+  
+  mail($to, $subject, $body, $header);
+  
+  if(!mail($to, $subject, $body, $header))
+    http_response_code(500);
 }
-
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email = strip_tags(htmlspecialchars($_POST['email']));
-$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-
-$to = "lkelengeshe@outlook.com"; 
-$subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
-
-mail($to, $subject, $body, $header);
-
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
-
 ?> 
 
