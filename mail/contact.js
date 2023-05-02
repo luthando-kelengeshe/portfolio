5aa8bf0f897cff5cdecb20b3a7e8e075b942f412
devotedmedia-admin <lkelengeshe@outlook.com>
2023-05-02 13:36:41 +0200

    Email Form

--- mail/contact.js
+++ mail/contact.js
@@ -1,7 +1,7 @@
 $(function () {
 
     $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
-        preventSubmit: false,
+        preventSubmit: true,
         submitError: function ($form, event, errors) {
         },
         submitSuccess: function ($form, event) {
@@ -29,7 +29,7 @@ $(function () {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
                     $('#success > .alert-success')
-                            .append("<strong>Thank you " + name + ", your message has been sent. </strong>");
+                            .append("<strong>Your message has been sent. </strong>");
                     $('#success > .alert-success')
                             .append('</div>');
                     $('#contactForm').trigger("reset");
@@ -38,7 +38,7 @@ $(function () {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Thank you " + name + ", it seems that my mail server is not responding. Please try again later!"));
+                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
                     $('#success > .alert-danger').append('</div>');
                     $('#contactForm').trigger("reset");
                 },
