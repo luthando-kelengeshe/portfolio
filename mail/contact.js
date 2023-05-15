c880e910e8df34eaf4c47c86651d32ab51560a9e
Luthando Kelengeshe <128071630+luthando-kelengeshe@users.noreply.github.com>
2023-05-15 12:38:08 +0200

    Update contact.js

--- mail/contact.js
+++ mail/contact.js
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "https://formsubmit.co/el/tifoka",
+                url: "https://formsubmit.co/luthandokelengeshe@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
