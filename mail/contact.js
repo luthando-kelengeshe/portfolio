8792b4c680652c9c06dc19aa4619ceb5c494c53a
Luthando Kelengeshe <128071630+luthando-kelengeshe@users.noreply.github.com>
2023-05-15 11:42:53 +0200

    Update contact.js
    Recipient email

--- mail/contact.js
+++ mail/contact.js
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "https://formsubmit.co/dde995377a7fb6e66620950e59821eb9",
+                url: "https://formsubmit.co/lkelengeshe@outlook.com",
                 type: "POST",
                 data: {
                     name: name,
